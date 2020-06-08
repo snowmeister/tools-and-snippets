419b9dd47a7b2d83206906926894cef80ce04e28
Mark Kennard <mark.kennard@gmail.com>
2020-06-08 15:57:14 +0000

    Modularised MongoTimeStampConvertor.js

--- MongoTimeStampConvertor.js
+++ MongoTimeStampConvertor.js
@@ -2,14 +2,11 @@
 /**
  * I'm certain there are other and better ways, but this is quick n durty...
  */
-
-function ObjectIdToDateTime(objectId) {
-  const timeStamp = parseInt(objectId.toString().substr(0, 8), 16) * 1000
+// ========
+module.exports = {
+  ObjectIdToDateTime: function (objectId) {
+    // whatever
+    const timeStamp = parseInt(objectId.toString().substr(0, 8), 16) * 1000
   return new Date(timeStamp)
-}
-
-// Stuff the ObjectID from Mongo here...
-const objectId = '5eda762fb9c1355c5fa715a4'
-
-// It wil be output to the console when this code runs...
-console.log(ObjectIdToDateTime(objectId))
+  }
+};
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-/**
- * Nothing to see here...
- */
+
+// Lets have a little demo for the MongoTimeStampConvertor
+const {ObjectIdToDateTime} = require('./MongoTimeStampConvertor');
+
+// Stuff the ObjectID from Mongo here...
+const objectId = '5eda762fb9c1355c5fa715a4'
+
+// It wil be output to the console when this code runs...
+console.log('ObjectIdToDateTime for ' +objectId+ ' \n' + ObjectIdToDateTime(objectId));
