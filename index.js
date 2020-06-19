85a3b01d09e92ab311a6d2698d1404b2c809c909
Mark Kennard <mark.kennard@threatstatus.com>
2020-06-19 09:50:45 +0100

    Added test data

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 
 const {arrQueue} = require('./constants')
 
-
+const {MQJ_DATA} = require('./mock-data/match-queue-jobs')
 // Lets have a little demo for the MongoTimeStampConvertor
 const {ObjectIdToDateTime} = require('./js/MongoTimeStampConvertor');
 const {TimeStampToDate} = require('./js/TimeStampToDate');
 
 
 // Lets walk over a bunch of jobs and compare the id datetimestamp to the actual job. They should be the same...
-arrQueue.forEach(queueItem=>{
+MQJ_DATA.forEach(queueItem=>{
   console.log('IDTIME:', ObjectIdToDateTime(queueItem._id), 'JOBTIME:', TimeStampToDate(queueItem.queue))
 })
 
